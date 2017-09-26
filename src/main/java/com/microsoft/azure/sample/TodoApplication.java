c25968131f35f30740fe808477849d0a66909b23
yungez <yungez@microsoft.com>
2017-09-26 13:48:40 +0800

    rename TodoAppJavaOnAzureApplication.java to TodoApplication.java (#17)
    * rename TodoAppJavaOnAzureApplication.java to TodoApplication.java
    
    * update readme
    
    * fix comments
    
    * fix typo, and add useful link

--- src/main/java/com/microsoft/azure/sample/TodoApplication.java
+++ src/main/java/com/microsoft/azure/sample/TodoApplication.java
@@ -9,9 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoAppJavaOnAzureApplication {
+public class TodoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TodoAppJavaOnAzureApplication.class, args);
+        SpringApplication.run(TodoApplication.class, args);
     }
 }
