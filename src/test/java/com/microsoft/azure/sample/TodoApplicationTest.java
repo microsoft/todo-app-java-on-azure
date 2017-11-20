15b6d100d0db53568b3e67646b0c34079689e5a7
Yan Zhang <yanzh@microsoft.com>
2017-11-20 10:12:54 +0800

    Test controller via WebMVCTest mode

--- src/test/java/com/microsoft/azure/sample/TodoApplicationTest.java
+++ src/test/java/com/microsoft/azure/sample/TodoApplicationTest.java
@@ -26,19 +26,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.invocation.InvocationOnMock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.microsoft.azure.sample.controller.TodoListController;
 import com.microsoft.azure.sample.dao.TodoItemRepository;
 import com.microsoft.azure.sample.model.TodoItem;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(TodoListController.class)
 public class TodoApplicationTest {
     static final String MOCK_ID = "mockId";
     static final String MOCK_DESC = "Mock Item";
